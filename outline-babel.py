2bb3d17625d6bc01ab868e03868821953d464205
Jonathan Cervidae <jonathan.cervidae@gmail.com>
2009-06-02 22:23:06 +0100

    Licenced under GPLv3

--- outline-babel.py
+++ outline-babel.py
@@ -3,7 +3,20 @@
 #
 # Author: Jonathan Cervidae <jonathan.cervidae@gmail.com>
 # PGP Fingerprint: 2DC0 0A44 123E 6CC2 EB55  EAFB B780 421F BF4C 4CB4
-# Last changed: $LastEdit: 2009-06-02 22:20:48 BST$
+# Last changed: $LastEdit: 2009-06-02 22:23:00 BST$
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import os
